a9d310b34196ef151d0a67e8d09da91d68423350
apfpo <apfpo2001@gmail.com>
2021-02-09 16:42:48 +0300

    add: add models

---
+++ Xdl.Internship.Offers.Models/TagModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class TagModel : BaseModel
+    {
+        public string Name { get; set; }
+        public int UsesByUser { get; set; }
+        public int UsesByVendor { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
