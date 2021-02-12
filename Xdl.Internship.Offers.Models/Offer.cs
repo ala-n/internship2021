7b3161b2780a6ddd98c391339a883d2262d1712c
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-12 14:06:55 +0300

    fix model

--- Xdl.Internship.Offers.Models/Offer.cs
+++ Xdl.Internship.Offers.Models/Offer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class OfferModel : BaseModel
+    public class Offer : BaseModel
     {
         public string Title { get; set; }
         public string Description { get; set; }
@@ -17,13 +17,8 @@ namespace Models
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public ObjectId VendorId { get; set; }
-        public bool IsActive { get; set; }
         public TagModel[] Tags { get; set; }
         public int NumberOfUses { get; set; }
         public int NumberOfViews { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public ObjectId CreatedBy { get; set; }
-        public ObjectId UpdatedBy { get; set; }
     }
 }
