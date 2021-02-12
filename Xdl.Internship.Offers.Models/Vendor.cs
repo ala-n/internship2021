a4676de251952e72d0d0cf53582b2cf67ab8a5ab
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-12 14:24:52 +0300

    fix model

---
+++ Xdl.Internship.Offers.Models/Vendor.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Vendor : BaseModel
+    {
+	public string Name { get; set; }
+	public string Title { get; set; }
+	public string Website { get; set; }
+	public string Description { get; set; 
+    }
+}
