dc4ace8b176abb848863abce0f430024785b7593
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-15 16:05:21 +0300

    add: add auditable model

---
+++ Xdl.Internship.Core/Xdl.Internship.Core.Models.MongoDB/AuditableModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace Xdl.Internship.Core.Models.MongoDB
+{
+    public abstract class AuditableModelBase : ModelBase, ICreationAwareModel, IUpdationAwareModel
+    {
+        public ObjectId CreatedBy { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public ObjectId UpdatedBy { get; set; }
+
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
