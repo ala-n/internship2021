8a8746f71efc054720d7a2ac53eef938d9f68267
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-15 16:28:09 +0300

    fix Vendor model

--- Xdl.Internship.Offers.Models/Vendor.cs
+++ Xdl.Internship.Offers.Models/Vendor.cs
@@ -5,13 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Xdl.Internship.Offers.Models
 {
-    public class Vendor : BaseModel
+    public class Vendor : AuditableModelBase
     {
 	public string Name { get; set; }
+	
 	public string Title { get; set; }
+	
 	public string Website { get; set; }
-	public string Description { get; set; 
+	
+	public string Description { get; set; }
+	
+	public double Rate { get; set; }
+	
+	public bool IsActive { get; set; }
     }
 }
