033e5d2cab5139d548082c61a92a6336279c7c09
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-15 16:25:13 +0300

    fix Tag model

--- Xdl.Internship.Offers.Models/Tag.cs
+++ Xdl.Internship.Offers.Models/Tag.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Xdl.Internship.Offers.Models
 {
-    public class Tag : BaseModel
+    public class Tag : AuditableModelBase
     {
         public string Name { get; set; }
+        
         public int UsesByUser { get; set; }
+        
         public int UsesByVendor { get; set; }
+        
+        public bool IsDeleted { get; set; }
     }
 }
