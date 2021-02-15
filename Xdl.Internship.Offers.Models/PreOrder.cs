8bb1d52fdd55a405008da1d692bb8dba894214b0
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-15 16:37:01 +0300

    add PreOrder model

---
+++ Xdl.Internship.Offers.Models/PreOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xdl.Internship.Offers.Models
+{
+    public class PreOrder : CreationAwareModel
+    {
+        public ObjectId OfferId { get; set; }
+
+        public ObjectId UserId { get; set; }
+
+        public ObjectId VendorEntityId { get; set; }
+
+        public string Description { get; set; }
+    }
+}
