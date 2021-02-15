40bc141e09a25ecda4f8e1a84ed75958e95b9002
apfpo <47034754+apfpo@users.noreply.github.com>
2021-02-15 16:22:02 +0300

    fix Feedback model

--- Xdl.Internship.Offers.Models/Feedback.cs
+++ Xdl.Internship.Offers.Models/Feedback.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Xdl.Internship.Offers.Models
 {
-    public class Feedback : BaseModel
+    public class Feedback :  AuditableModelBase
     {
         public ObjectId OfferId { get; set; }
+        
         public ObjectId UserId { get; set; }
+        
+        public ObjectId VendorEntityId { get; set; }
+        
         public int Rate { get; set; }
     }
 }
