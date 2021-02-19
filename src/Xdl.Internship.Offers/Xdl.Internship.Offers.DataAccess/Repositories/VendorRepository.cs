4973766255d7148d084d8be90e04356c11673889
renewss <2ibragimov01@gmail.com>
2021-02-19 20:59:10 +0500

    add Vendor repo && update local dependencies

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DataAccess/Repositories/VendorRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xdl.Internship.Core.DataAccess.MongoDB.CollectionProviders;
+using Xdl.Internship.Core.DataAccess.MongoDB.Repositories;
+using Xdl.Internship.Offers.Models;
+
+namespace Xdl.Internship.Offers.DataAccess.Repositories
+{
+    //public interface IVendorRepository
+    //{
+
+    //}
+
+    public class VendorRepository : MongoRepositoryBase<Vendor>
+    {
+        public VendorRepository(ICollectionProvider collectionProvider)
+            : base(collectionProvider)
+        {
+        }
+    }
+}
