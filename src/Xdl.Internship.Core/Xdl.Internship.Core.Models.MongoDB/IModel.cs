0edc5a1bdc4a8a78f813ae44850765d75b35d2f5
Misha <creatorlogic1@gmail.com>
2021-02-21 02:21:23 +0200

    Revert "deleted files"

---
+++ src/Xdl.Internship.Core/Xdl.Internship.Core.Models.MongoDB/IModel.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson;
+
+namespace Xdl.Internship.Core.Models.MongoDB
+{
+    public interface IModel
+    {
+        ObjectId Id { get; set; }
+    }
+}
---
+++ src/Xdl.Internship.Core/Xdl.Internship.Core.Models.MongoDB/UpdationAwareModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Xdl.Internship.Core.Models.MongoDB
+{
+    public interface IUpdationAwareModel
+    {
+        string UpdatedBy { get; set; }
+
+        DateTimeOffset UpdatedAt { get; set; }
+    }
+
+    public abstract class UpdationAwareModel : ModelBase, IUpdationAwareModel
+    {
+        public string UpdatedBy { get; set; }
+
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
