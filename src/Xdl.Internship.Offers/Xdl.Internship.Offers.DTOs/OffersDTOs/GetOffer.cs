64f20d16db3ec0febcae037a526317cf0ed04dd9
apfpo <apfpo2001@gmail.com>
2021-02-25 20:24:53 +0300

    add DTOs

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DTOs/OffersDTOs/GetOffer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xdl.Internship.Core.Models.MongoDB;
+
+namespace Xdl.Internship.Offers.DTOs.OffersDTOs
+{
+    public class GetOffer : ModelBase
+    {
+        public string Title { get; set; }
+
+        public string VendorName { get; set; }
+    }
+}
---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DTOs/VendorDTOs/GetVendor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xdl.Internship.Core.Models.MongoDB;
+
+namespace Xdl.Internship.Offers.DTOs.VendorDTOs
+{
+    public class GetVendor : ModelBase
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string WebSite { get; set; }
+    }
+}
