250df150750219bdc98477b6021c87da31c8dc95
Misha <creatorlogic1@gmail.com>
2021-02-25 18:24:41 +0200

    change rabbit

--- src/Xdl.Internship.Core/Xdl.Internship.Core.RabbitMQ/CheckExpiredOffersMessagePublisher.cs
+++ src/Xdl.Internship.Core/Xdl.Internship.Core.RabbitMQ/CheckExpiredOffersMessagePublisher.cs
@@ -13,6 +13,11 @@ namespace Xdl.Internship.Core.RabbitMQ.SDK
             _queueName = rabbitMqOptions.Value.QueueName; 
         }
 
+        public void Publish(byte[] body)
+        {
+            _modelAccessor.Value.BasicPublish(exchange: _queueName, routingKey: _queueName, basicProperties: null, body: body);
+        }
+
         protected override void DeclareQueue(IModel model)
         {
             model.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
