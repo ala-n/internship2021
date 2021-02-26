3edce4a17d17ad3e31fa26f4fc7faae7e0eb57fe
apfpo <apfpo2001@gmail.com>
2021-02-26 10:00:31 +0300

    add dtos

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DTOs/OfferDTOs/OfferMainDTO.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+
+namespace Xdl.Internship.Offers.DTOs.OffersDTOs
+{
+    public class OfferMainDTO
+    {
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string VendorName { get; set; }
+
+        public int NumberOfViews { get; set; }
+
+        public int NumberOfUses { get; set; }
+
+        public string Discount { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DTOs/VendorDTOs/VendorMainDTO.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+
+namespace Xdl.Internship.Offers.DTOs.VendorDTOs
+{
+    public class VendorMainDTO
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Website { get; set; }
+
+        public string Description { get; set; }
+    }
+}
