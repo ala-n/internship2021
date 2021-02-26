0c93cffd05a72e7155bc5cc31e39d984b7901548
Misha <creatorlogic1@gmail.com>
2021-02-26 16:07:59 +0200

    tags changes

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DTOs/TagDTOs/TagDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace Xdl.Internship.Offers.DTOs.TagDTOs
+{
+    public class TagDTO
+    {
+        public ObjectId Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        public string Name { get; set; }
+
+        public int UsesByUser { get; set; }
+
+        public int UsesByVendor { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
--- src/Xdl.Internship.Offers/Xdl.Internship.Offers.DataAccess/Repositories/TagRepository.cs
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.DataAccess/Repositories/TagRepository.cs
@@ -1,14 +1,27 @@
-﻿using Xdl.Internship.Core.DataAccess.MongoDB.CollectionProviders;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xdl.Internship.Core.DataAccess.MongoDB.CollectionProviders;
 using Xdl.Internship.Core.DataAccess.MongoDB.Repositories;
+using Xdl.Internship.Offers.DataAccess.Interfaces;
 using Xdl.Internship.Offers.Models;
 
 namespace Xdl.Internship.Offers.DataAccess.Repositories
 {
-    public class TagRepository : MongoRepositoryBase<Tag>
+    public class TagRepository : MongoRepositoryBase<Tag>, ITagRepository
     {
         public TagRepository(ICollectionProvider collectionProvider)
             : base(collectionProvider)
         {
+        }
+
+        public async Task<ICollection<Tag>> FindTopTagsAsync()
+        {
+            Expression<Func<Tag, bool>> filter = (tag) => tag.UsesByUser >= 10;
+
+            return await FindAsync(filter);
         }
     }
 }
