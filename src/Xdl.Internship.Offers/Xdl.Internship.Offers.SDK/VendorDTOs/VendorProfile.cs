5681148af0f43f3206760db01c1ddad4d5fa1f61
renewss <2ibragimov01@gmail.com>
2021-02-28 20:23:39 +0500

    remove test logger

--- src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/VendorDTOs/VendorProfile.cs
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/VendorDTOs/VendorProfile.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using AutoMapper;
+﻿using AutoMapper;
 using Xdl.Internship.Offers.Models;
 using Xdl.Internship.Offers.SDK.VendorEntityDTOs;
 
@@ -9,7 +8,6 @@ namespace Xdl.Internship.Offers.SDK.VendorDTOs
     {
         public VendorProfile()
         {
-            Console.WriteLine(11);
             CreateMap<Vendor, VendorDTO>();
 
             CreateMap<VendorDTO, VendorWithEntitiesDTO>();
