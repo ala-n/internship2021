89c74516c8bd0893c0494d75d19475e63dfcbfed
renewss <2ibragimov01@gmail.com>
2021-03-01 01:29:30 +0500

    add profiles for automapper

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/AddressDTOs/AddressProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Xdl.Internship.Offers.Models;
+
+namespace Xdl.Internship.Offers.SDK.AddressDTOs
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<Adress, AddressDTO>();
+        }
+    }
+}
---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/CityDTOs/CityProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Xdl.Internship.Offers.Models;
+
+namespace Xdl.Internship.Offers.SDK.CityDTOs
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<City, CityDTO>();
+        }
+    }
+}
