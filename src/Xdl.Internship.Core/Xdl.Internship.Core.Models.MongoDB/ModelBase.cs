59791b85991e4f47637fd966acb43b62f6204562
renewss <2ibragimov01@gmail.com>
2021-03-04 01:04:26 +0500

    update base model

--- src/Xdl.Internship.Core/Xdl.Internship.Core.Models.MongoDB/ModelBase.cs
+++ src/Xdl.Internship.Core/Xdl.Internship.Core.Models.MongoDB/ModelBase.cs
@@ -1,9 +1,11 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Xdl.Internship.Core.Models.MongoDB
 {
     public abstract class ModelBase : IModel
     {
+        [BsonId]
         public ObjectId Id { get; set; }
     }
 }
