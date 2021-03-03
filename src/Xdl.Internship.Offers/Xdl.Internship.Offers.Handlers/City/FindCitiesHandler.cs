3464a56cbfdc4190dbee09ca8c424fea83c69e8c
renewss <2ibragimov01@gmail.com>
2021-03-03 21:37:56 +0500

    add handler for city

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.Handlers/City/FindCitiesHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using MongoDB.Bson;
+using Xdl.Internship.Offers.DataAccess.Interfaces;
+using Xdl.Internship.Offers.DataAccess.Repositories;
+using Xdl.Internship.Offers.Models;
+using Xdl.Internship.Offers.SDK.CityDTOs;
+
+namespace Xdl.Internship.Offers.Handlers.City
+{
+    public class FindCitiesHandler : IRequestHandler<FindCitiesRequest, ICollection<CityDTO>>
+    {
+        private readonly IVendorRepository _vendorRepository;
+        private readonly IMapper _mapper;
+
+        public FindCitiesHandler(VendorRepository vendorRepository, IMapper mapper)
+        {
+            _vendorRepository = vendorRepository;
+            _mapper = mapper;
+        }
+
+        public Task<ICollection<CityDTO>> Handle(FindCitiesRequest request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.Handlers/City/FindCitiesRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Xdl.Internship.Offers.SDK.CityDTOs;
+
+namespace Xdl.Internship.Offers.Handlers.City
+{
+    public class FindCitiesRequest : IRequest<ICollection<CityDTO>>
+    {
+        public bool IncludeInactive { get; }
+
+        public FindCitiesRequest(bool includeInactive)
+        {
+            IncludeInactive = includeInactive;
+        }
+    }
+}
