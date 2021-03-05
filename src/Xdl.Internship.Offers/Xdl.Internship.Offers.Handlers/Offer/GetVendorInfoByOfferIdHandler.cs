8078288b4815c01f967f66919ec7301e36e133a6
apfpo <apfpo2001@gmail.com>
2021-03-05 16:08:19 +0300

    add methods

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.Handlers/Offer/GetVendorInfoByOfferIdHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Xdl.Internship.Offers.DataAccess.Interfaces;
+using Xdl.Internship.Offers.DataAccess.Repositories;
+using Xdl.Internship.Offers.SDK.VendorDTOs;
+
+namespace Xdl.Internship.Offers.Handlers.Offer
+{
+    public class GetVendorInfoByOfferIdHandler : IRequestHandler<GetVendorInfoByOfferIdRequest, VendorInfoForOfferDTO>
+    {
+        private readonly IOfferRepository _offerRepository;
+        private readonly IVendorEntityRepository _vendorEntityRepository;
+        private readonly IVendorRepository _vendorRepository;
+        private readonly IMapper _mapper;
+
+        public GetVendorInfoByOfferIdHandler(OfferRepository offerRepository, VendorEntityRepository vendorEntityRepository, VendorRepository vendorRepository,
+            IMapper mapper)
+        {
+            _offerRepository = offerRepository;
+            _vendorEntityRepository = vendorEntityRepository;
+            _vendorRepository = vendorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<VendorInfoForOfferDTO> Handle(GetVendorInfoByOfferIdRequest request, CancellationToken cancellationToken)
+        {
+            var result = new List<VendorInfoForOfferDTO>();
+            var offer = await _offerRepository.FindOfferById(request.OfferId);
+            var entity = await _vendorEntityRepository.FindByIdAsync(offer.VendorEntitiesId.First());
+            var vendor = await _vendorRepository.FindByIdAsync(entity.VendorId);
+            return _mapper.Map<VendorInfoForOfferDTO>(vendor);
+        }
+    }
+}
