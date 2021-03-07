d75343cc5ce864b2356c88ec7f59f7a9fd489f75
DoniyorJN <umidshoot@gmail.com>
2021-03-07 23:39:52 +0500

    add auth validation

---
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xdl.Internship.Offers.SDK
+{
+    public class AppSettings
+    {
+        public string SecretKey { get; set; }
+    }
+}
