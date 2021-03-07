636218ba58c23046a18ad7a55d782954877b68ac
Misha <creatorlogic1@gmail.com>
2021-03-07 11:35:04 +0200

    added RabbitMQ nuget

---
+++ src/Xdl.Internship.Core/Xdl.Internsip.Core.RabbitMQ/MessagePublisher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace Xdl.Internship.Core.RabbitMQ
+{
+    public class MessagePublisher : PublisherBase, IRabbitMqPublisher
+    {
+        private readonly string _queueName;
+
+        public MessagePublisher(IOptions<RabbitMqConfiguration> rabbitMqOptions)
+            : base(rabbitMqOptions)
+        {
+            _queueName = rabbitMqOptions.Value.QueueName;
+        }
+
+        public void Publish(byte[] body)
+        {
+            _modelAccessor.Value.BasicPublish(exchange: string.Empty, routingKey: _queueName, basicProperties: null, body: body);
+        }
+
+        protected override void DeclareQuee(IModel model)
+        {
+            model.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+    }
+}
---
+++ src/Xdl.Internship.Core/Xdl.Internsip.Core.RabbitMQ/MessageSender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Xdl.Internship.Core.RabbitMQ
+{
+    public class MessageSender : IMessageSender<MessageDTO>
+    {
+        private readonly IRabbitMqPublisher _publisher;
+
+        public MessageSender(IRabbitMqPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public Task PublishAsync(MessageDTO message)
+        {
+            string jsonView = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(jsonView);
+
+            _publisher.Publish(body);
+
+            return Task.CompletedTask;
+        }
+    }
+}
