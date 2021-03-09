3659df4cd6c14f0e6cdfeb5bb936c9447d757384
renewss <2ibragimov01@gmail.com>
2021-03-09 21:50:51 +0500

    add explicit city mapping

--- src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/CityDTOs/CityProfile.cs
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/CityDTOs/CityProfile.cs
@@ -8,11 +8,19 @@ namespace Xdl.Internship.Offers.SDK.CityDTOs
     {
         public CityProfile()
         {
-            CreateMap<City, CityDTO>();
+            CreateMap<City, CityDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+                .ForMember(dest => dest.UpdatedBy, opt => opt.MapFrom(src => src.UpdatedBy))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt));
 
             CreateMap<CreateCityDTO, City>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTimeOffset.Now))
-                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTimeOffset.Now));
 
             CreateMap<UpdateCityDTO, City>()
                 .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTimeOffset.Now))
--- src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/CityDTOs/CreateCityDTO.cs
+++ src/Xdl.Internship.Offers/Xdl.Internship.Offers.SDK/CityDTOs/CreateCityDTO.cs
@@ -3,5 +3,7 @@
     public class CreateCityDTO
     {
         public string Name { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
